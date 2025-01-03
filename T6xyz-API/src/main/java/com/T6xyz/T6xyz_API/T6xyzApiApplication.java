9e458f0aad7f3aef422107b9a675c676d9bff83b
T6xyz <benjaminzwolenik@gmail.com>
2025-01-02 21:16:42 -0600

    first commit

---
+++ T6xyz-API/src/main/java/com/T6xyz/T6xyz_API/T6xyzApiApplication.java
@@ -0,0 +1,13 @@
+package com.T6xyz.T6xyz_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T6xyzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T6xyzApiApplication.class, args);
+	}
+
+}
---
+++ T6xyz-API/src/test/java/com/T6xyz/T6xyz_API/T6xyzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T6xyz.T6xyz_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T6xyzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
