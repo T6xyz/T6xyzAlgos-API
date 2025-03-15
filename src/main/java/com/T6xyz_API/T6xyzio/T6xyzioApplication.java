3dc4e3da380ea80140e875d73fe5aa6ff5e633da
T6xyz <benjaminzwolenik@gmail.com>
2025-03-15 02:12:32 -0400

    first backend commit

---
+++ src/main/java/com/T6xyz_API/T6xyzio/T6xyzioApplication.java
@@ -0,0 +1,13 @@
+package com.T6xyz_API.T6xyzio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T6xyzioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T6xyzioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/T6xyz_API/T6xyzio/T6xyzioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T6xyz_API.T6xyzio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T6xyzioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
