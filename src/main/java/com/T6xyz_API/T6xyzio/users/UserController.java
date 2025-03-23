bc3293d2c05970d4502c5a45d3b49a746dd1c576
T6xyz <benjaminzwolenik@gmail.com>
2025-03-23 18:27:47 -0400

    Added getCSRF() for testing

--- src/main/java/com/T6xyz_API/T6xyzio/users/UserController.java
+++ src/main/java/com/T6xyz_API/T6xyzio/users/UserController.java
@@ -2,17 +2,26 @@ package com.T6xyz_API.T6xyzio.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepo userRepo;
 
+    @GetMapping("/getCSRF")
+    public CsrfToken getCSRF(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
     @PostMapping("/addUser")
     public ResponseEntity<String> addUser() {
-        UserDTO testUser = new UserDTO("test", "test", "test", false, false, false);
+        UserDTO testUser = new UserDTO("Da weed", "test", "test", false, false, false);
         userRepo.save(testUser);
         return ResponseEntity.ok("Response");
     }
